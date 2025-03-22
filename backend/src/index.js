1b1f6fd88e7c37df6f2e71f83815bae10cf11e5f
sakanaowo <idiotbugg@gmail.com>
2025-03-22 18:53:24 +0700

    nothing here meng

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
-import appRoutes from "./routes/auth.route.js";
-import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import { connectDB } from "./lib/db.js";
+
+import appRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+
+
+
 dotenv.config();
 
 const PORT = process.env.PORT;
@@ -13,8 +19,7 @@ app.use(express.json()); // for parsing application/json
 app.use(cookieParser());// allow to parse cookies in middleware
 
 app.use("/api/auth", appRoutes);
-
-
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port:" + PORT);

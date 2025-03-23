a326f1e38eeec4676006e1cfb6b5c51a02ae3daf
sakanaowo <idiotbugg@gmail.com>
2025-03-23 23:16:20 +0700

    Add CORS middleware to enable frontend-backend communication
    Integrated CORS middleware to allow requests from the frontend running on localhost:5173. Configured CORS to include credentials for authenticated requests. This update facilitates seamless communication between the frontend and backend services.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
 import { connectDB } from "./lib/db.js";
 
 import appRoutes from "./routes/auth.route.js";
@@ -17,6 +19,10 @@ const app = express();
 
 app.use(express.json()); // for parsing application/json
 app.use(cookieParser());// allow to parse cookies in middleware
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  credentials: true
+}));// allow to make request from frontend to backend
 
 app.use("/api/auth", appRoutes);
 app.use("/api/message", messageRoutes);

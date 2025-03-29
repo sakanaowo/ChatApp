2f78d7ed2d44b59f78bd4080939b186fcb259fc3
sakanaowo <idiotbugg@gmail.com>
2025-03-30 00:36:44 +0700

    Update theme support and fix profile routing logic
    Add dynamic theme handling by integrating `useThemeStore` and setting `data-theme` attribute. Fix incorrect conditional rendering for the profile route to ensure authenticated access only.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,9 +10,12 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { useEffect } from 'react';
 import { Loader } from "lucide-react";
 import { Toaster } from 'react-hot-toast';
+import { useThemeStore } from './store/useThemeStore.js';
 
 const App = () => {
-  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
+  const { theme } = useThemeStore();
+  console.log({ onlineUsers });
 
   useEffect(() => {
     checkAuth();
@@ -27,14 +30,14 @@ const App = () => {
   }
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
       <Routes>
         <Route path='/' element={authUser ? <HomepPage /> : <Navigate to="/login" />} />
         <Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
         <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
         <Route path='/settings' element={<SettingsPage />} />
-        <Route path='/profile' element={!authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+        <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
       <Toaster />
     </div>

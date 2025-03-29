c8406ef9c20376938980bf3546476f63f1c8d9af
sakanaowo <idiotbugg@gmail.com>
2025-03-30 00:31:26 +0700

    Refactor axios instance configuration for environment support

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
 
-const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5001/api/',
+export const axiosInstance = axios.create({
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
     withCredentials: true,
 });
-export default axiosInstance;

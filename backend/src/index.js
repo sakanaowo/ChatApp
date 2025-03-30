276224a049a910bd91ee402bcb9189f3be5aacd4
sakanaowo <idiotbugg@gmail.com>
2025-03-30 09:48:46 +0700

    Add static file serving for production build
    Implemented static file serving for the frontend in production mode using Express and path. Ensured all unmatched routes serve the React app's index.html to support client-side routing. This only activates when NODE_ENV is set to "production".

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
+import path from "path";
 import { connectDB } from "./lib/db.js";
 
 import appRoutes from "./routes/auth.route.js";
@@ -12,6 +12,7 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json()); // for parsing application/json
 app.use(cookieParser());// allow to parse cookies in middleware
@@ -22,6 +23,12 @@ app.use(cors({
 
 app.use("/api/auth", appRoutes);
 app.use("/api/messages", messageRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("Server is running on port:" + PORT);

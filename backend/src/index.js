da576a5891cc6ad2d1fda47f4e8ddbdc3611cede
sakanaowo <idiotbugg@gmail.com>
2025-03-30 17:37:34 +0700

    vl

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,9 +24,9 @@ app.use(cors({
 app.use("/api/auth", appRoutes);
 app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 

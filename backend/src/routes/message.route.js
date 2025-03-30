21c5335eb696ae8d57b7f4bc04da2dcabd05a0fc
sakanaowo <idiotbugg@gmail.com>
2025-03-30 09:48:26 +0700

    fix typo

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getUserForSidebar, getMessages, sendMessage } from "../controllers/message.controller.js";
+import { getUsersForSidebar, getMessages, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
-router.get("/users", protectRoute, getUserForSidebar);
+router.get("/users", protectRoute, getUsersForSidebar);
 router.get("/:id", protectRoute, getMessages);
 
 router.post("/send/:id", protectRoute, sendMessage);

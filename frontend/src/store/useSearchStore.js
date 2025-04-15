507294b52966b18c34d49532b65a2d869d686c39
sakanaowo <idiotbugg@gmail.com>
2025-04-15 21:23:57 +0700

    Thêm thanh search bar thiếu logic tìm kiếm

---
+++ frontend/src/store/useSearchStore.js
@@ -0,0 +1,15 @@
+// This file contains the Zustand store for managing search results and loading state.
+import { create } from "zustand";
+
+// export const useSearchStore = create((set, get) => ({
+//     searchResults: [],
+//     isLoading: false,
+//     searchTerm: "",
+//     setSearchTerm: (term) => set({ searchTerm: term }),
+//     setSearchResults: (results) => set({ searchResults: results }),
+//     setIsLoading: (loading) => set({ isLoading: loading }),
+// }));
+export const useSearchStore = create((set) => ({
+    searchTerm: "",
+    setServers: (term) => set({ searchTerm: term }),
+}));

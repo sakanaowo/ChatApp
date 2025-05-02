78a55276c4072f723938cd4358e989080e77a96a
Dang Khoa <hnkhoa04@gmail.com>
2025-05-02 09:07:25 +0700

    change userid in email model into not unique

--- backend/src/controllers/friendRequest.controller.js
+++ backend/src/controllers/friendRequest.controller.js
@@ -35,6 +35,7 @@ export const sendRequest = async (req, res) => {
             return res.status(400).json({ message: "You cannot send a friend request to yourself" });
         }
 
+        //Lấy email trên mongoDB
         const fromUser = await Email.findOne({ email: fromEmail });
         const toUser = await Email.findOne({ email: toEmail });
 
@@ -290,3 +291,5 @@ export const listPendingRequests = async (req, res) => {
         res.status(500).json({ message: "Internal Server Error" });
     }
 };
+
+//Lấy tất cả người dùng ở các server

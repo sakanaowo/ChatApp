acdd8431752ff218c352ffb360eda8dab19db15c
sakanaowo <idiotbugg@gmail.com>
2025-05-15 22:32:33 +0700

    Refactor friend request routes for clarity and new feature.
    Updated route paths to improve clarity and consistency. Added a new endpoint to retrieve a list of sent friend requests. This enhances the API's usability and organization.

--- backend/src/routes/friendRequest.route.js
+++ backend/src/routes/friendRequest.route.js
@@ -4,6 +4,7 @@ import express from "express";
 import {
     sendRequest,         // gửi lời mời kết bạn
     listPendingRequests, // lấy danh sách lời mời
+    listSendedRequests,  // lấy danh sách lời mời đã gửi
     acceptRequest,       // chấp nhận lời mời
     rejectRequest        // từ chối lời mời
 } from "../controllers/friendRequest.controller.js";
@@ -12,10 +13,13 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 // Gửi lời mời kết bạn
-router.post("/", protectRoute, sendRequest);
+router.post("/send", protectRoute, sendRequest);
 
 // Lấy danh sách lời mời kết bạn chưa xử lý
-router.get("/", protectRoute, listPendingRequests);
+router.get("/pending", protectRoute, listPendingRequests);
+
+// Lấy danh sách lời mời kết bạn đã gửi
+router.get("/sended", protectRoute, listSendedRequests);
 
 // Chấp nhận lời mời kết bạn
 // để như này để test api dễ hơn
